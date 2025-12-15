17a2142a55735e51027d9cc440e211a30a7cc158
UltralyticsAssistant <web@ultralytics.com>
2025-12-15 04:31:13 +0000

    Auto-format by https://ultralytics.com/actions

--- downloadCoCo8.py
+++ downloadCoCo8.py
@@ -1,4 +1,7 @@
+# Ultralytics 🚀 AGPL-3.0 License - https://ultralytics.com/license
+
 from datasets import load_dataset
+
 dataset = load_dataset("Ultralytics/COCO8")
 # 数据会加载到内存，或保存到本地
 dataset.save_to_disk("path/to/coco8")
