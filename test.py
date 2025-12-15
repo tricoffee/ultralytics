86c04caebc3dbc5115521fe36b46c7a09318b6c2
UltralyticsAssistant <web@ultralytics.com>
2025-12-15 04:31:07 +0000

    Auto-format by https://ultralytics.com/actions

--- test.py
+++ test.py
@@ -1,23 +1,25 @@
+# Ultralytics 🚀 AGPL-3.0 License - https://ultralytics.com/license
+
 import os
-os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
+
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
 from ultralytics import YOLO
 
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     # Load a pretrained YOLO11n model
     model = YOLO("/hy-tmp/ultralytics/runs/segment/train19/weights/best.pt")
 
     # Train the model on the COCO8 dataset for 100 epochs
-    #train_results = model.train(
+    # train_results = model.train(
     #    data="Custom-seg.yaml",  # Path to dataset configuration file
-     #   epochs=100,  # Number of training epochs
-      #  imgsz=640,  # Image size for training
-        # device="cpu",  # Device to run on (e.g., 'cpu', 0, [0,1,2,3])
-       # device=0,  # Device to run on (e.g., 'cpu', 0, [0,1,2,3])
-        #batch=64,  # batchSize
-        #workers=8,  #
-    #)
+    #   epochs=100,  # Number of training epochs
+    #  imgsz=640,  # Image size for training
+    # device="cpu",  # Device to run on (e.g., 'cpu', 0, [0,1,2,3])
+    # device=0,  # Device to run on (e.g., 'cpu', 0, [0,1,2,3])
+    # batch=64,  # batchSize
+    # workers=8,  #
+    # )
 
     # Evaluate the model's performance on the validation set
     metrics = model.val()
