698560da13d3c0c6a3051e295a418dd1ccf93a46
UltralyticsAssistant <web@ultralytics.com>
2026-03-06 04:11:31 +0000

    Auto-format by https://ultralytics.com/actions

--- downloadCoCo8.py
+++ downloadCoCo8.py
@@ -1,4 +1,5 @@
 from datasets import load_dataset
+
 dataset = load_dataset("Ultralytics/COCO8")
 # 数据会加载到内存，或保存到本地
 dataset.save_to_disk("path/to/coco8")
--- train.py
+++ train.py
@@ -1,10 +1,10 @@
 import os
-os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
+
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
 from ultralytics import YOLO
 
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     # Load a pretrained YOLO11n model
     model = YOLO("yolo11m-seg.pt")
 
@@ -17,33 +17,29 @@ if __name__ == '__main__':
         device=0,  # Device to run on (e.g., 'cpu', 0, [0,1,2,3])
         batch=64,  # batchSize
         workers=8,  #
-        mosaic=0.0,    # 关闭 Mosaic（4图拼接）
-        mixup=0.0,     # 关闭 MixUp（图像混合）
-        copy_paste=0.0,# 可选：也关闭 Copy-Paste
-        augment=True,           # 必须开启
-        auto_augment=None,      # 关闭 randaugment
-
-    # 颜色
-    hsv_h=0.015,
-    hsv_s=0.7,
-    hsv_v=0.4,
-
-    # 几何
-    degrees=10.0,
-    translate=0.1,
-    scale=0.9,              # 强烈推荐！比 0.5 更好
-    shear=2.0,
-    perspective=0.0,        # 分割任务关闭
-
-    # 翻转
-    fliplr=0.5,
-    flipud=0.5,
-
-    # 拼接
-   # mosaic=0.0,
-   # mixup=0.0,              # 少量有益
-   # copy_paste=0.0,
-    # close_mosaic=0,        # 必须！最后10轮关闭
+        mosaic=0.0,  # 关闭 Mosaic（4图拼接）
+        mixup=0.0,  # 关闭 MixUp（图像混合）
+        copy_paste=0.0,  # 可选：也关闭 Copy-Paste
+        augment=True,  # 必须开启
+        auto_augment=None,  # 关闭 randaugment
+        # 颜色
+        hsv_h=0.015,
+        hsv_s=0.7,
+        hsv_v=0.4,
+        # 几何
+        degrees=10.0,
+        translate=0.1,
+        scale=0.9,  # 强烈推荐！比 0.5 更好
+        shear=2.0,
+        perspective=0.0,  # 分割任务关闭
+        # 翻转
+        fliplr=0.5,
+        flipud=0.5,
+        # 拼接
+        # mosaic=0.0,
+        # mixup=0.0,              # 少量有益
+        # copy_paste=0.0,
+        # close_mosaic=0,        # 必须！最后10轮关闭
     )
 
     # Evaluate the model's performance on the validation set
